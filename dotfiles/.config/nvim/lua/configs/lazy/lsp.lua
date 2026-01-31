50e9a4d4110a22e4b932f686d2c3314030e4eed4
Sahitya Uniyal <sahityauniyal@gmail.com>
2026-01-31 14:55:03 +0530

    update the lsp configs to one place

--- dotfiles/.config/nvim/lua/configs/lazy/lsp.lua
+++ dotfiles/.config/nvim/lua/configs/lazy/lsp.lua
@@ -1,22 +1,174 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            vim.lsp.enable('pyright')
-            vim.lsp.enable('gopls')
-            vim.lsp.enable('lua_ls')
+	"neovim/nvim-lspconfig",
+	lazy = false,
+	dependencies = {
+		"stevearc/conform.nvim",
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"j-hui/fidget.nvim",
+		"rafamadriz/friendly-snippets",
+		"nvimtools/none-ls.nvim",
+		"jayp0521/mason-null-ls.nvim",
+	},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {},
+		})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { noremap = true })
-            vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { noremap = true })
+		-- Additional diagnostics and code actions
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.code_actions.gomodifytags,
+			},
+		})
 
-            -- Configure diagnostics for floating window
-            vim.keymap.set("n", "<leader>dw", function()
-                vim.diagnostic.open_float(nil, { focus = false })
-            end, {})
-        end,
-    },
+		-- Completion setup
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
+
+		require("luasnip.loaders.from_vscode").lazy_load()
+
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
+
+		-- progress notification
+		require("fidget").setup({})
+
+		-- Language Server Installer
+		require("mason").setup()
+		-- foramtter/linter
+		require("mason-null-ls").setup({
+			ensure_installed = {
+				"stylua",
+				"prettier",
+				-- Go
+				"golangci_lint",
+				"goimports",
+				"gomodifytags",
+				-- Python Formatters
+				"black", -- Auto-format Python code (PEP8 compliant")
+				"isort", -- Sort import"s
+			},
+		})
+		-- LSP Server Setup
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"gopls",
+				"pyright",
+				"gopls",
+				"harper_ls",
+			},
+			handlers = {
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+				["lua_ls"] = function()
+					local lspconfig = require("lspconfig")
+
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								runtime = {
+									version = "LuaJIT",
+								},
+								diagnostics = {
+									globals = { "vim" },
+								},
+								workspace = {
+									library = vim.api.nvim_get_runtime_file("", true),
+									checkThirdParty = false,
+								},
+								format = {
+									enable = true,
+									-- Put format options here
+									-- NOTE: the value should be STRING!!
+									defaultConfig = {
+										indent_style = "space",
+										indent_size = "2",
+									},
+								},
+							},
+						},
+					})
+				end,
+			},
+		})
+
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			}),
+			sources = cmp.config.sources({
+				{ name = "copilot", group_index = 2 },
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- For luasnip users.
+			}, {
+				{ name = "buffer" },
+			}),
+		})
+
+		-- Diagnostics Configuration
+		vim.diagnostic.config({
+			-- update_in_insert = true,
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+			},
+		})
+
+		-- LSP Key bindings
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+		vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+		vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { noremap = true })
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { noremap = true })
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		-- Configure diagnostics for floating window
+		vim.keymap.set("n", "<leader>dw", function()
+			vim.diagnostic.open_float(nil, { focus = false })
+		end, {})
+
+		-- Auto format on save
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
+	end,
 }
