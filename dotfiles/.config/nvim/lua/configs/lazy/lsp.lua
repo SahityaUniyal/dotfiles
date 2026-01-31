4669bd701463aa7dc3d2ac0687f2c581ea2adf9f
Sahitya Uniyal <sahityauniyal@gmail.com>
2026-01-31 21:37:31 +0530

    add goimports formatter

--- dotfiles/.config/nvim/lua/configs/lazy/lsp.lua
+++ dotfiles/.config/nvim/lua/configs/lazy/lsp.lua
@@ -26,6 +26,7 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+				null_ls.builtins.formatting.goimports,
 				null_ls.builtins.diagnostics.golangci_lint,
 				null_ls.builtins.code_actions.gomodifytags,
 			},
