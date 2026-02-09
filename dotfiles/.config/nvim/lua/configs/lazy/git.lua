20235d21cd8d494f3066de84733b208ab5a98af4
Sahitya Uniyal <sahityauniyal@gmail.com>
2026-02-09 20:16:23 +0530

    add git plugins

---
+++ dotfiles/.config/nvim/lua/configs/lazy/git.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+			vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+			vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+
+			vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>hs", ":Gitsigns stage_hunk<CR>", {})
+		end,
+	},
+}
