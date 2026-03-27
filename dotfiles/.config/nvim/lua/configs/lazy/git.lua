bb96858da7ecb2d45784b608f9070c5b855f9de0
Sahitya Uniyal <sahityauniyal@gmail.com>
2026-03-27 21:01:58 +0530

    Gitsigns key maps update

--- dotfiles/.config/nvim/lua/configs/lazy/git.lua
+++ dotfiles/.config/nvim/lua/configs/lazy/git.lua
@@ -10,10 +10,59 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
-			require("gitsigns").setup()
+			local gitsigns = require("gitsigns")
+			gitsigns.setup({
+				word_diff = false,
+				on_attach = function(bufnr)
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
 
-			vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
-			vim.keymap.set("n", "<leader>hs", ":Gitsigns stage_hunk<CR>", {})
+					-- Navigation
+					map("n", "]c", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "]c", bang = true })
+						else
+							gitsigns.nav_hunk("next")
+						end
+					end)
+
+					map("n", "[c", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "[c", bang = true })
+						else
+							gitsigns.nav_hunk("prev")
+						end
+					end)
+
+					-- Actions
+					map("n", "<leader>hs", gitsigns.stage_hunk)
+					map("n", "<leader>hr", gitsigns.reset_hunk)
+
+					map("v", "<leader>hs", function()
+						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+
+					map("v", "<leader>hr", function()
+						gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end)
+
+					map("n", "<leader>hp", gitsigns.preview_hunk)
+					map("n", "<leader>hi", gitsigns.preview_hunk_inline)
+
+					map("n", "<leader>hb", function()
+						gitsigns.blame_line({ full = true })
+					end)
+
+					map("n", "<leader>hd", gitsigns.diffthis)
+
+					-- Toggles
+					map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+					map("n", "<leader>tw", gitsigns.toggle_word_diff)
+				end,
+			})
 		end,
 	},
 }
