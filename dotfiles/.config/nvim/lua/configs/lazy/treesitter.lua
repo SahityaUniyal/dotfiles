6a7af65ceac58ca042930df4161a12bb5c8b8e1d
Sahitya Uniyal <sahityauniyal@gmail.com>
2026-05-02 20:13:47 +0530

    treesitter config update

--- dotfiles/.config/nvim/lua/configs/lazy/treesitter.lua
+++ dotfiles/.config/nvim/lua/configs/lazy/treesitter.lua
@@ -1,77 +1,82 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		tag = "v0.10.0",
 		lazy = false,
 		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				-- A list of parser names, or "all"
-				ensure_installed = {
-					"vimdoc",
-					"javascript",
-					"c",
-					"lua",
-					"go",
-					"python",
-					"jsdoc",
-					"bash",
-				},
-
-				-- Install parsers synchronously (only applied to `ensure_installed`)
-				sync_install = false,
-
-				-- Automatically install missing parsers when entering buffer
-				-- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-				auto_install = true,
-
-				indent = {
-					enable = true,
-				},
-
-				highlight = {
-					-- `false` will disable the whole extension
-					enable = true,
-					disable = function(lang, buf)
-						if lang == "html" then
-							return true
-						end
-
-						local max_filesize = 100 * 1024 -- 100 KB
-						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-						if ok and stats and stats.size > max_filesize then
-							vim.notify(
-								"File larger than 100KB treesitter disabled for performance",
-								vim.log.levels.WARN,
-								{ title = "Treesitter" }
-							)
-							return true
-						end
-					end,
-
-					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-					-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-					-- Using this option may slow down your editor, and you may see some duplicate highlights.
-					-- Instead of true it can also be a list of languages
-					additional_vim_regex_highlighting = { "markdown" },
-				},
+			require("nvim-treesitter").setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
 			})
 
-			local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-			treesitter_parser_config.templ = {
-				install_info = {
-					url = "https://github.com/vrischmann/tree-sitter-templ.git",
-					files = { "src/parser.c", "src/scanner.c" },
-					branch = "master",
-				},
-			}
+			-- Install parsers (no-op if already installed)
+			require("nvim-treesitter").install({
+				"vimdoc",
+				"javascript",
+				"c",
+				"lua",
+				"go",
+				"python",
+				"jsdoc",
+				"bash",
+			})
 
-			vim.treesitter.language.register("templ", "templ")
+			-- Enable treesitter highlighting via autocommand
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "*" },
+				callback = function(args)
+					local ft = vim.bo[args.buf].filetype
+					-- Disable for html
+					if ft == "html" then
+						return
+					end
+					pcall(vim.treesitter.start)
+				end,
+			})
+
+			-- Enable treesitter-based indentation
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "*" },
+				callback = function()
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+
+			-- Enable treesitter-based folding
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "*" },
+				callback = function()
+					vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					vim.wo[0][0].foldmethod = "expr"
+				end,
+			})
+
+			-- For markdown, also enable syntax highlighting alongside treesitter
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "markdown" },
+				callback = function()
+					vim.cmd("syntax on")
+				end,
+			})
+
+			-- Example: Adding custom parser (commented out)
+			-- vim.api.nvim_create_autocmd("User", {
+			-- 	pattern = "TSUpdate",
+			-- 	callback = function()
+			-- 		require("nvim-treesitter.parsers").templ = {
+			-- 			install_info = {
+			-- 				url = "https://github.com/vrischmann/tree-sitter-templ.git",
+			-- 				branch = "master",
+			-- 			},
+			-- 		}
+			-- 	end,
+			-- })
+			-- vim.treesitter.language.register("templ", "templ")
 		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
-		after = "nvim-treesitter",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		lazy = false,
 		config = function()
 			require("treesitter-context").setup({
 				enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
